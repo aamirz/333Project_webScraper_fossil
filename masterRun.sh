0d8542aba28a6cf3c1a8b11364f95a38a26a80ce
Ubuntu <ubuntu@ip-172-31-7-80.ec2.internal>
2017-05-05 06:30:01 +0000

    reconfigured master run for better structure, now writes to data/logs/

--- masterRun.sh
+++ masterRun.sh
@@ -19,12 +19,14 @@ HOUR="$(date "+%H")"
 # now make sure that we run for both today and yesterday
 TODAY="$(date "+%Y_%m_%d")"
 DAY="$(date "+%m/%d/%Y")"
-./pullToday.sh $TODAY $DAY > ./data/log_"${TODAY}"_$HOUR
+./pullToday.sh $TODAY $DAY > ./data/logs/log_"${TODAY}"_$HOUR
 
+# for testing
+#echo "I love you" > ./data/logs/log_"${TODAY}"_$HOUR
 
 # run yesterday 
 TODAY="$(date -d "yesterday 13:00" "+%Y_%m_%d")"
 DAY="$(date -d "yesterday 13:00" "+%m/%d/%Y")"
-./pullToday.sh $TODAY $DAY > ./data/log_"${TODAY}"_$HOUR
+./pullToday.sh $TODAY $DAY > ./data/logs/log_"${TODAY}"_$HOUR
 
 exit 0
