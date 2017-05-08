802188fb32dc9cf63a1645d11ab14afa82b7f014
Ubuntu <ubuntu@ip-172-31-7-80.ec2.internal>
2017-05-08 20:30:42 +0000

    refill reconfigured

--- refill.sh
+++ refill.sh
@@ -16,29 +16,29 @@ cd prowler_webScraper/
 # for log file index
 HOUR="$(date "+%H")"
 
-# # now make sure that we run for both today and yesterday
-# TODAY="$(date "+%Y_%m_%d")"
-# DAY="$(date "+%m/%d/%Y")"
-# ./pullToday.sh $TODAY $DAY > ./logs/log_"${TODAY}"_$HOUR
+# now make sure that we run for both today and yesterday
+TODAY="$(date "+%Y_%m_%d")"
+DAY="$(date "+%m/%d/%Y")"
+./pullToday.sh $TODAY $DAY > ./logs/log_"${TODAY}"_$HOUR
 
-# # for testing
-# #echo "I love you" > ./data/logs/log_"${TODAY}"_$HOUR
+# for testing
+#echo "I love you" > ./data/logs/log_"${TODAY}"_$HOUR
 
-# # run yesterday 
-# TODAY="$(date -d "yesterday 13:00" "+%Y_%m_%d")"
-# DAY="$(date -d "yesterday 13:00" "+%m/%d/%Y")"
-# ./pullToday.sh $TODAY $DAY > ./logs/log_"${TODAY}"_$HOUR
+# run yesterday 
+TODAY="$(date -d "yesterday 13:00" "+%Y_%m_%d")"
+DAY="$(date -d "yesterday 13:00" "+%m/%d/%Y")"
+./pullToday.sh $TODAY $DAY > ./logs/log_"${TODAY}"_$HOUR
 
 
 # now run 19th Aril - 30 April
 YEAR="2017"
 MONTH="04"
-# for daynum in {19..30}
-# do
-# TODAY="${YEAR}_${MONTH}_${daynum}"
-# DAY="${MONTH}/${daynum}/${YEAR}"
-# ./pullToday.sh $TODAY $DAY > ./logs/log_"${TODAY}"_$HOUR
-# done
+for daynum in {20..30}
+do
+TODAY="${YEAR}_${MONTH}_${daynum}"
+DAY="${MONTH}/${daynum}/${YEAR}"
+./pullToday.sh $TODAY $DAY > ./logs/log_"${TODAY}"_$HOUR
+done
 
 MONTH="05"
 lowDays=("01" "02" "03" "04" "05" "06" "07" "08" "09")
