6dc13d7c0569ad9b92e978f8e4490fb4dd1c8f90
Aamir Zainulabadeen <aamirz@princeton.edu>
2017-05-08 15:47:19 -0400

    refill reconfigured to work for low number days in May

--- refill.sh
+++ refill.sh
@@ -35,11 +35,28 @@ for daynum in {19..30}
 do
 TODAY="${YEAR}_${MONTH}_${daynum}"
 DAY="${MONTH}/${daynum}/${YEAR}"
-#echo $TODAY
-#echo $DAY
-# TODAY="$(date -d "yesterday 13:00" "+%Y_%m_%d")"
-# DAY="$(date -d "yesterday 13:00" "+%m/%d/%Y")"
 ./pullToday.sh $TODAY $DAY > ./logs/log_"${TODAY}"_$HOUR
 done
+
+
+lowDays=("01" "02" "03" "04" "05" "06" "07" "08" "09")
+# now pull for the month of may 
+for day in ${lowDays[@]}
+do 
+TODAY="${YEAR}_${MONTH}_${daynum}"
+DAY="${MONTH}/${daynum}/${YEAR}"
+./pullToday.sh $TODAY $DAY > ./logs/log_"${TODAY}"_$HOUR
+done
+
+# now pull the beginning of may
+MONTH="05"
+for day in ${lowDays[@]}
+do 
+TODAY="${YEAR}_${MONTH}_${daynum}"
+DAY="${MONTH}/${daynum}/${YEAR}"
+./pullToday.sh $TODAY $DAY > ./logs/log_"${TODAY}"_$HOUR
+done
+
+
 
 exit 0
