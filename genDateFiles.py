f2341b9454b952f1349d152fb62284f37b77b60f
Aamir Zainulabadeen <aamirz@princeton.edu>
2017-05-09 00:26:34 -0400

    generated the date files for pulling prince

--- genDateFiles.py
+++ genDateFiles.py
@@ -10,40 +10,18 @@ The command line arguments are 1: PATH and 2: year.
 """
 import sys
 import os
+from datetime import date, datetime, timedelta
 
-# format a day, s is either '/' or '_'
-def formatDay(m, d, y, s):
-    return m + s + d + s + y
+def perdelta(start, end, delta):
+    curr = start
+    while curr < end:
+        yield curr
+        curr += delta
 
-def formatDir(m, d, y, s):
-    return y + s + m + s + d
-
-# generate properly formatted dates
-# for each day in the year
+# print a range of dates
 def main():
-    if len(sys.argv) < 3:
-        print "BAD ARGUMENTS"
-        return 0
-
-
-    # pull out the arguments
-    savePath = str(sys.argv[1])
-    year = str(sys.argv[2])
-
-    # months
-    months = ['01']#, '02', '03', '04']
-    base = ['01', '02', '03', '04', '05', '06', '07', '08', '09']
-    dRange = range(10, 32)
-    next = [str(d) for d in dRange]
-    days = base + next
-
-
-    with open("dates.txt", "w") as outF:
-        for m in months:
-            for d in days:
-                #print formatDay(m, d, year, '/') + " " + formatDay(m, d, year, '_')
-                outF.write(formatDay(m, d, year, '/') + " " + formatDir(m, d, year, '_') + "\n")
-
+    for result in perdelta(date(2015, 01, 01), date(2017, 03, 31), timedelta(days=1)):
+        print result.strftime("%Y_%m_%d") + "\t" + result.strftime("%m/%d/%Y")
 
 # run main
 if __name__=="__main__":
